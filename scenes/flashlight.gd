dbaed021e8e8d526f4d6a4b3c57b75dd54570850
adamhendrickson22 <189288438+aethyrius@users.noreply.github.com>
2026-01-31 14:20:44 -0500

    Added the flashlight again

---
+++ scenes/flashlight.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+@onready var flashlight: SpotLight3D = $SpotLight3D
+@export var canvas: CanvasLayer
+@export var texture_outer: TextureRect
+@export var texture_inner: TextureRect
+
+var _wide_range = 10
+var _wide_energy = 5
+var _wide_angle = 90
+var _wide_scale = 1.5
+
+var _tight_range = 40
+var _tight_energy = 20
+var _tight_angle = 20
+var _tight_scale = 0.5
+
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("flashlight")):
+		flashlight.visible = !flashlight.visible
+		canvas.visible = !canvas.visible
+		
+	# Widen light
+	if (Input.is_action_just_pressed("flashlight_scroll_up")):
+		flashlight.light_energy = lerpf(flashlight.light_energy, _wide_energy, 0.5)
+		flashlight.spot_angle = lerpf(flashlight.spot_angle, _wide_angle, 0.5)
+		flashlight.spot_range = lerpf(flashlight.spot_range, _wide_range, 0.5)
+		texture_outer.scale.x = lerpf(texture_outer.scale.x, _wide_scale, 0.5)
+		texture_outer.scale.y = lerpf(texture_outer.scale.y, _wide_scale, 0.5)
+		texture_inner.rotation_degrees = lerpf(texture_inner.rotation_degrees, 90, 0.5)		
+
+		
+	# Tighten light
+	elif (Input.is_action_just_pressed("flashlight_scroll_down")):
+		flashlight.light_energy = lerpf(flashlight.light_energy, _tight_energy, 0.5)
+		flashlight.spot_angle = lerpf(flashlight.spot_angle, _tight_angle, 0.5)
+		flashlight.spot_range = lerpf(flashlight.spot_range, _tight_range, 0.5)
+		texture_outer.scale.x = lerpf(texture_outer.scale.x, _tight_scale, 0.5)
+		texture_outer.scale.y = lerpf(texture_outer.scale.y, _tight_scale, 0.5)
+		texture_inner.rotation_degrees = lerpf(texture_inner.rotation_degrees, 0, 0.5)	
+		
+	
