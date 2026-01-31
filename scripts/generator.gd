819f1ade10eb7b982f6041a9f4aa2b8d585d79b4
kevinyan0 <55335508+kevinyan0@users.noreply.github.com>
2026-01-30 23:52:50 -0500

    tyuhuhuy

---
+++ scripts/generator.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+
+@export var defect : PackedScene
+@export var defect_position_padding : float
+
+
+func _ready() -> void:
+	
+	var repair_plane = $RepairPlane
+	
+	
+	for i in 7:
+		
+		var aabb = repair_plane.get_aabb()
+		var size = repair_plane.get_aabb().size
+		
+		var defect_instance = defect.instantiate()
+		
+		print(aabb.position)
+		
+		defect_instance.position =  repair_plane.position + aabb.position + Vector3(randf_range(defect_position_padding,size.x - defect_position_padding), randf_range(defect_position_padding,size.y - defect_position_padding), 0)
+		
+		print(defect_instance.position)
+		
+		add_child(defect_instance)
