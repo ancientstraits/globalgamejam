2f142347a1bf1bcaf650052893f22b5a681bbf29
kevinyan0 <55335508+kevinyan0@users.noreply.github.com>
2026-01-31 12:47:03 -0500

    fix defects

---
+++ scenes/interaction_cast.gd
@@ -0,0 +1,9 @@
+extends ShapeCast3D
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("action1"):
+		var col : Node3D
+		
+		for i in get_collision_count():
+			col = get_collider(i)
+			col.get_parent().interacting = true
