b60236c2de6ddb12717d26de0f67c9b8579771a5
kevinyan0 <55335508+kevinyan0@users.noreply.github.com>
2026-01-31 17:30:22 -0500

    fixes

--- scripts/player3d.gd
+++ scripts/player3d.gd
@@ -17,9 +17,6 @@ var hang_pos: Vector3 = Vector3.ZERO
 var has_postproc: bool
 var postproc: CanvasLayer = null
 
-var hanging: bool = false
-var hang_pos: Vector3 = Vector3.ZERO
-
 # var camera_velocity: Vector2
 	
 func _ready() -> void:
