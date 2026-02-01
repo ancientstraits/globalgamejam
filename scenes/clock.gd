35c522605de1f8f1a4624e495ea438cfaf15df98
Anon Ymous <ancientstraits@protonmail.com>
2026-02-01 13:43:03 -0500

    improved clock

--- scenes/clock.gd
+++ scenes/clock.gd
@@ -2,12 +2,14 @@ extends StaticBody3D
 
 @export var time_stretch : int
 
-@onready var label = $Label3D
+@onready var labels: Array[Label3D] = [$Label3DFront, $Label3DBack]
 
-func game_end() -> bool:
-	var hour : int
-	hour = int(floor(Globals.time)) / (60 * time_stretch)
-	return hour >= 6
+func _set_label_text(text: String):
+	for label in labels:
+		label.text = text
+
+func is_game_over(hour: int, minutes: int) -> bool:
+	return minutes >= 10
 
 func _process(delta: float) -> void:
 	Globals.time += delta
@@ -17,6 +19,10 @@ func _process(delta: float) -> void:
 	minutes = (int(floor(Globals.time)) % (60 * time_stretch)) / time_stretch
 	
 	if minutes < 10:
-		label.text = str(hour) + ':0' + str(minutes)
+		_set_label_text(str(hour) + ':0' + str(minutes))
 	else:
-		label.text = str(hour) + ':' + str(minutes)
+		_set_label_text(str(hour) + ':' + str(minutes))
+	
+	if is_game_over(hour, minutes):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		get_tree().change_scene_to_file('res://scenes/win_screen.tscn')
